575d3db676861b8cf4ff0beeea4328fe553d3cd0
KrisP <Kris.Piessens@naturalsciences.be>
2019-01-30 19:44:07 +0100

    Speed comparison done and ok

--- ELL/ELL/ELL.cpp
+++ ELL/ELL/ELL.cpp
@@ -3,7 +3,28 @@
 
 #include "stdafx.h"
 
+struct typTmp {
+	int a = 0;
+	int b = 0;
+	double c = 0;
+	int l = 0;
+};
+
 int __stdcall ellTest(int i) {
 	i++;
 	return i;
+}
+
+double __stdcall ellPassStruct(typTmp &tmp) {
+	double r;
+	r = tmp.a + tmp.b + tmp.c;
+	return r;
+}
+
+double __stdcall ellPassStruct02(typTmp &tmp) {
+	double r;
+	for (int i = 0; i < tmp.l; i++) {
+		r = tmp.a + tmp.b + tmp.c;
+	}
+	return r;
 }
